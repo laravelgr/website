43ff78f31b8fb84f367b354bdcab739feea95b9a
Drakakis George <drakakisgeo@users.noreply.github.com>
2025-03-29 19:16:37 +0200

    code comment for shorturl config

--- config/shortcuts.php
+++ config/shortcuts.php
@@ -1,5 +1,8 @@
 <?php
 
+ /**
+ * Warning, url is like this for example -> laravel.gr/s/subscribe
+ */
  return [
    'urls' => [
        'subscribe' => 'https://mailchi.mp/f98a09aa59f4/laravel-community-sync'
